8a8164420c55461bdc167bd955385739d13695f3
Anastasia <aardiss@mail.ru>
2020-08-20 16:00:34 +0300

    админ-панель

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="app-container">
     <headline title="Панель администрирования">
-      <h3>Content</h3>
+      <user />
     </headline>
+    <navigation />
   </div>
 </template>
 
 <script>
 import headline from "./components/headline";
+import user from "./components/user";
+import navigation from "./components/navigation";
 
 
 export default {
   components: {
     headline,
+    user,
+    navigation,
   },
 };
 </script>
