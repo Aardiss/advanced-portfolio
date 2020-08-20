0afddfb71fe9920fdfa10385884fd288bb5dcb9a
Anastasia <aardiss@mail.ru>
2020-08-20 14:15:11 +0300

    шапка страницы

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,27 @@
-<template lang="pug">
-  h1 Welcome to the Vue App
+<template>
+  <div class="app-container">
+    <headline title="Панель администрирования">
+      <h3>Content</h3>
+    </headline>
+  </div>
 </template>
+
+<script>
+import headline from "./components/headline";
+
+
+export default {
+  components: {
+    headline,
+  },
+};
+</script>
+
+<style lang="postcss">
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800');
+  @import "normalize.css";
+  @import "../styles/mixins.pcss";
+  @import "../styles/layout/base.pcss";
+</style>
+
+<style lang="postcss" scoped src="./app.pcss"></style>
